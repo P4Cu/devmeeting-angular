63c8c84b38d0c0f96d64f2b7f0fe94de0b580652
Andrzej Pacanowski <andrzej.pacanowski@gmail.com>
2018-10-06 11:52:21 +0200

    use/implement promoted-product

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ProductComponent } from '../product/product.component'
+import { PromotedProductComponent } from '../promoted-product/promoted-product.component'
 import { Product } from '../models'
 
 @Component({
--- src/app/promoted-product/promoted-product.component.ts
+++ src/app/promoted-product/promoted-product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../models'
 
 @Component({
   selector: 'app-promoted-product',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./promoted-product.component.css']
 })
 export class PromotedProductComponent implements OnInit {
+
+  @Input() element : Product;
 
   constructor() { }
 
