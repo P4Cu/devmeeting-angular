a7cd7a2f835738d5606687bc423a5018fa8841be
Andrzej Pacanowski <andrzej.pacanowski@gmail.com>
2018-10-06 13:11:09 +0200

    generate filter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { PromotedProductComponent } from './promoted-product/promoted-product.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ProductListComponent,
-    PromotedProductComponent
+    PromotedProductComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
