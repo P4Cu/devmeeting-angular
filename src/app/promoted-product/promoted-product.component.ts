bdc984ffddcfed231e3e2cfdeafe18e67789f160
Andrzej Pacanowski <andrzej.pacanowski@gmail.com>
2018-10-06 11:45:29 +0200

    generate promoted-product component

---
+++ src/app/promoted-product/promoted-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promoted-product',
+  templateUrl: './promoted-product.component.html',
+  styleUrls: ['./promoted-product.component.css']
+})
+export class PromotedProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
